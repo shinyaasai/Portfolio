0db1e84bd19d120127393e70a93881a56dea3b49
EC2 Default User <ec2-user@ip-172-31-42-185.ec2.internal>
2020-03-15 10:36:09 +0000

    検索機能追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,4 +2,12 @@ class Record < ApplicationRecord
   enum sun: { yes: 0, no: 1 }
   enum medicine: { 飲んだ: 0, 飲まなかった: 1 }
   enum awakening: { good: 0, normal: 1, bad: 2 }  
+  
+  def self.search(search)
+      if search
+        Record.where(['created_at LIKE ?', "%#{search}%"])
+      else
+        Record.all
+      end
+    end
 end
