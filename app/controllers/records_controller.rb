7a38426321a96cd63d13aef8c6141d7dac3e4dda
EC2 Default User <ec2-user@ip-172-31-42-185.ec2.internal>
2020-03-15 08:58:15 +0000

    newページ　indexページの実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,10 +1,15 @@
 class RecordsController < ApplicationController
-  
+  require 'date'
   def index
+    @records = Record.all
   end
   
   def new
     @record = Record.new
+  end
+  
+  def show
+    @record = Record.find(params[:id])
   end
   
   def create
@@ -17,4 +22,6 @@ class RecordsController < ApplicationController
     def record_params
       params.require(:record).permit(:getup_time, :sleep_time, :awakening, :medicine, :sun, :memo)
     end
+    
+    
 end
--- app/models/record.rb
+++ app/models/record.rb
@@ -1,2 +1,5 @@
 class Record < ApplicationRecord
+  enum sun: { yes: 0, no: 1 }
+  enum medicine: { 飲んだ: 0, 飲まなかった: 1 }
+  enum awakening: { good: 0, normal: 1, bad: 2 }  
 end
