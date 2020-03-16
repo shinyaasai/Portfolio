5968bd7eb738fbbc71dcd40ffcfa067588f93028
EC2 Default User <ec2-user@ip-172-31-42-185.ec2.internal>
2020-03-16 09:03:40 +0000

    ウィザードフォーム実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,10 +1,15 @@
 class RecordsController < ApplicationController
   require 'date'
-  def index
+  def done
     @records = Record.all.search(params[:search])
   end
   
-  def new
+  def form1
+    session[:memo] = record_params[:memo]
+    @record = Record.new
+  end
+  
+  def form2
     @record = Record.new
   end
   
@@ -13,15 +18,19 @@ class RecordsController < ApplicationController
   end
   
   def create
-    @record = Record.create(record_params)
+    @record = Record.new(
+                      memo: session[:memo],
+                      sun: record_params[:sun]
+                      )
+    @record.save                  
     flash[:notice] = "登録しました"
-    redirect_to records_path 
+    redirect_to done_records_path
   end
   
     private
    
     def record_params
-      params.require(:record).permit(:getup_time, :sleep_time, :awakening, :medicine, :sun, :memo)
+      params.require(:record).permit(:getup_time, :sleep_time, :memo, :sun)
     end
     
     
