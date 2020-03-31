fd5c240e495a4e55aadc571bd0ad34114d7ed0d5
EC2 Default User <ec2-user@ip-172-31-42-185.ec2.internal>
2020-03-31 22:56:11 +0000

    アカウント編集画面にユーザーネーム追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :user_name, presence: true, length: { maximum: 10 }       
+  
   has_many :records, dependent: :destroy      
 end
