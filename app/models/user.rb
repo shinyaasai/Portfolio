1781ab692d91ed1c4e3728f13ea37ff5ee087e94
EC2 Default User <ec2-user@ip-172-31-42-185.ec2.internal>
2020-03-31 22:46:23 +0000

    ユーザーネームカラムにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  validates :user_name, presence: true, length: { maximum: 10 }       
   has_many :records, dependent: :destroy      
 end
