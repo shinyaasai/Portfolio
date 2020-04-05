c9e23cbaa87b7a353c50a00a37ca01f34a55cc33
EC2 Default User <ec2-user@ip-172-31-42-185.ec2.internal>
2020-04-05 08:16:59 +0000

    定型コードが自動作成されないようにapplication.rbを編集

--- config/application.rb
+++ config/application.rb
@@ -1,22 +1,23 @@
 require_relative 'boot'
-
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SleepApp
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
     config.load_defaults 5.1
     config.i18n.default_locale = :ja
     config.time_zone = "Tokyo"
     config.active_record.time_zone_aware_types = [:datetime, :time]
     config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
      html_tag
+    end
+    config.generators do |g| 
+      g.test _framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
     end
   end
 end
