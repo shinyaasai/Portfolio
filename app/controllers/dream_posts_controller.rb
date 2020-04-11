8038db2e0d15e899ccb0d8213951daf43b9c33e7
EC2 Default User <ec2-user@ip-172-31-42-185.ec2.internal>
2020-04-11 17:20:27 +0000

    投稿ボタンを非同期に実装

--- app/controllers/dream_posts_controller.rb
+++ app/controllers/dream_posts_controller.rb
@@ -5,7 +5,7 @@ class DreamPostsController < ApplicationController
     @dream_post = current_user.dream_posts.build(dream_post_params)
     @record = @dream_post.record
     if @dream_post.save
-      redirect_to root_path
+      respond_to :js
     end
       
    
@@ -15,7 +15,7 @@ class DreamPostsController < ApplicationController
     @dream_post = DreamPost.find_by(id: params[:id])
     @record = @dream_post.record
     if @dream_post.destroy
-      redirect_to root_path
+      respond_to :js
     end
     
   end
