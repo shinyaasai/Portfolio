6f3a3a8ebc264d5d6cbd8cd69a74f52a3761afea
浅井伸哉 <agu09z003@gmail.com>
2020-04-16 01:40:54 +0900

    record_spec.rbに検索のテストを追加

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -1,53 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Record, type: :model do
-  # before do
-  #   @user = User.create(
-  #   user_name: "ボブ",
-  #   email: "bobishandsum@example.com",
-  #   password: "bobisplaingsoccer",
-  #   )
-  it "起床時間、就寝時間、薬、目覚め、布団から出る、朝日、眠気を登録できること"  do
-     expect(create(:record)).to be_valid
+  context "ユーザーがログインしている時" do
+
+    it "起床時間、就寝時間、薬、目覚め、布団から出る、朝日、眠気を登録できること"  do
+       expect(create(:record)).to be_valid
+    end
   end
 
-  it "目覚めの項目を入力しなければ登録ができない" do
+  context "ユーザーがログインしていない時" do
+
+    it "起床時間、就寝時間、薬、目覚め、布団から出る、朝日、眠気を登録できないこと" do
+      record = Record.create(
+        getup_time: Time.current,
+        sleep_time: Time.current,
+        medicine: "飲んだ",
+        awakening: "起きた",
+        getout: "１０分",
+        sun: "浴びた",
+        sleepiness: "元気に過ごせた",
+      )
+      expect(record).to_not be_valid
+    end
+  end
+
+  it "目覚めの項目を入力しなければ登録ができないこと" do
     record = build(:record, sleepiness: nil)
     record.valid?
     expect(record.errors[:sleepiness]).to include("を入力してください")
   end
 
-  it "目覚めの項目を入力しなければ登録ができない" do
+  it "目覚めの項目を入力しなければ登録ができないこと" do
     record = build(:record, sun: nil)
     record.valid?
     expect(record.errors[:sun]).to include("を入力してください")
   end
 
-  it "目覚めの項目を入力しなければ登録ができない" do
+  it "目覚めの項目を入力しなければ登録ができないこと" do
     record = build(:record, getout: nil)
     record.valid?
     expect(record.errors[:getout]).to include("を入力してください")
   end
 
-  it "目覚めの項目を入力しなければ登録ができない" do
+  it "目覚めの項目を入力しなければ登録ができないこと" do
     record = build(:record, awakening: nil)
     record.valid?
     expect(record.errors[:awakening]).to include("を入力してください")
   end
 
-  it "薬の項目を入力しなければ登録ができない" do
+  it "薬の項目を入力しなければ登録ができないこと" do
     record = build(:record, medicine: nil)
     record.valid?
     expect(record.errors[:medicine]).to include("を入力してください")
   end
 
-  it "起床時間に未来日は選択できない" do
+  it "起床時間に未来日は選択できないこと" do
     record = build(:record, getup_time: Time.current + 1,)
     record.valid?
     expect(record.errors[:getup_time]).to include("に未来日は選択できません")
   end
 
-  it "起床時間に未来日は選択できない" do
+  it "起床時間に未来日は選択できないこと" do
     record = build(:record, sleep_time: Time.current + 1,)
     record.valid?
     expect(record.errors[:sleep_time]).to include("に未来日は選択できません")
@@ -56,5 +69,21 @@ RSpec.describe Record, type: :model do
   it "夢の記録は空でも投稿ができること" do
     record = build(:record, memo: "")
     expect(record).to be_valid
+  end
+
+  describe "日付に一致する記録を検索する" do
+    let(:record) { create(:record) }
+
+    context "一致する記録が見つかる時" do
+      it "検索した日付に一致する日付を返すこと" do
+        expect(Record.search ("浴びた")).to include(record)
+      end
+    end
+
+    context "一致する記録が見つからない時" do
+      it "空のコレクションを返すこと" do
+        expect(Record.search ("浴びた")).to be_empty
+      end
+    end
   end
 end
