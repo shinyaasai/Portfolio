78bbc876dd817fc624f94cc5fcc8768f23473883
浅井伸哉 <agu09z003@gmail.com>
2020-04-25 19:31:51 +0900

    睡眠時間を今月のデータで表示できるように実装

--- app/controllers/user_statuses_controller.rb
+++ app/controllers/user_statuses_controller.rb
@@ -3,5 +3,9 @@ class UserStatusesController < ApplicationController
     user = User.find_by(params[user_id: current_user.id])
     @records = user.records
     @average = 0
+    @this_month = Date.today.all_month
+    start_date = Time.current.beginning_of_month
+    end_date = Time.current.end_of_month
+    @this_month_record = @records.where(getup_time: start_date..end_date).group('MONTH(getup_time)').count
   end
 end
